28e0d2169400591414bdc0ca3912c9e336e81f3c
vildanovrr <vildanovrr@sgaz.pro>
2023-09-07 13:45:27 +0500

    add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,14 +4,17 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/SideBar/ui';
 
 const App = () => {
-  const {theme, toggleTheme} = useTheme();
+  const {theme} = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar/>
-      <button onClick={toggleTheme}>toggle</button>
-      <AppRouter />
+      <div className='content-page'>
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
