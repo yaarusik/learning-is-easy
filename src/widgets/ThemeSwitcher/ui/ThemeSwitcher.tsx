a375b225ac37639d02421e5265fc3d731fdb3d54
vildanovrr <vildanovrr@sgaz.pro>
2023-09-07 13:48:32 +0500

    add icons

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './themeSwitcher.module.scss';
+import React from 'react';
+import { Theme, useTheme } from 'app/providers/ThemeProvider';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+
+interface ThemeSwitcherProps {
+  className?: string;
+}
+export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
+  const {theme, toggleTheme} = useTheme();
+
+  return (
+    <Button
+      theme={ThemeButton.CLEAR}
+      className={classNames(cls.themeSwitcher, {}, [className])}
+      onClick={toggleTheme}
+    >
+      {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+    </Button>
+  );
+};
