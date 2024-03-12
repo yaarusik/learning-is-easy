c83983ffb88a8231e1eb64d07efce8ce6b58bf54
evocargo <ruslan.vildanov@evocargo.com>
2024-03-12 14:39:24 +0500

    fix lint errors

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink to="/" theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>Главная</AppLink>
-            <AppLink to="/about" theme={AppLinkTheme.RED}>О сайте</AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    return (
+
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink to="/" theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>
+                    {t('Главная')}
+                </AppLink>
+                <AppLink to="/about" theme={AppLinkTheme.RED}>{t('О сайте')}</AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
