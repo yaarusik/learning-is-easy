19a21d93edaf75262f240f1c9c700c57ffd0bc1d
evocargo <ruslan.vildanov@evocargo.com>
2024-04-01 12:20:27 +0500

    add loki

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { BugButton } from 'app/providers/ErrorBoundary';
 
 const MainPage = () => {
     const { t } = useTranslation();
